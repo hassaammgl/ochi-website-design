cddaed2c79a9511ba3077032a0438efc06520150
hassaammgl <h455a4mmehtab@gmail.com>
2025-05-24 01:46:43 +0500

    feat: add LandingPage component and update Navbar with links; modify styles in index.css

---
+++ src/components/LandingPage.jsx
@@ -0,0 +1,14 @@
+
+const LandingPage = () => {
+    return (
+        <div className="bg-zinc-900 w-full h-screen pt-1">
+            <div className="textstructure mt-40 px-20">
+                <div className="masker">
+                    <h1 className="uppercase text-9xl font-founders-grotesk font-medium tracking-tighter leading-none">We Create</h1>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default LandingPage
